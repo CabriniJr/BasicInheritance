ef6b82fbb000e0285e84f935e6a40e033ab85d7b
Luigi <luigi.mendes.cabrini@outlook.com>
2025-09-02 21:48:13 -0300

    ex1

--- BasicInheritance/Program.cs
+++ BasicInheritance/Program.cs
@@ -1,3 +1,8 @@
 ﻿using BasicInheritance;
 
 Console.WriteLine("******Herança básica********\n");
+//Fazendo um objeto carro 
+
+Car myCar = new Car(80) { Speed = 50 };
+
+Console.WriteLine($"Carro está a {myCar.Speed} km/h");
